595ad41947c053178ddb6545f4efa68a7ae79adc
kimjihye058 <137058764+kimjihye058@users.noreply.github.com>
2023-10-07 22:49:31 +0900

    [unrated] Title: 특수문자 출력하기, Time: 112.01 ms, Memory: 58.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+                
+        System.out.println("!@#$%^&*(\\\'\"<>?:;");
+        //`'`와 `"` 문자를문자열 내에서 사용하려면 역슬래시(`\`)를 이용하여 이스케이프 처리
+        //역슬래시(`\`)도 이스케이프 처리해야 합니다. 역슬래시는 이스케이프 문자로 사용되므로,
+        //역슬래시를 문자열 내에 포함시키려면 `\\`로 표현
+
+                                                                                                  
+    }
+}
