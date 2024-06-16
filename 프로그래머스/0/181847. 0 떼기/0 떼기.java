0f8561d96faf4d2685e789a2571d80a190330794
kimjihye058 <137058764+kimjihye058@users.noreply.github.com>
2024-06-17 08:36:57 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        // 정수로 바뀐 값을 담기 위해 num 변수 선언
+        int num = Integer.parseInt(n_str);  
+        // 다시 answer에 문자열로 바꿔서 담기
+        return answer = String.valueOf(num);
+    }
+}
