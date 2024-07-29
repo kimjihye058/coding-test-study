ba02735d99e9c7367f8e1561708262fd228fc419
김지혜 <137058764+kimjihye058@users.noreply.github.com>
2024-07-30 00:00:10 +0900

    [level 0] Title: 이어 붙인 수, Time: 3.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    answer = 0
+    a=""#홀수
+    b=""#짝수
+    for i in num_list:
+        if i%2!=0:
+            a+=str(i)
+        else:
+            b+=str(i)
+    return int(a)+int(b)
