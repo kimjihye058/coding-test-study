dd0ade2c453a52e0c837d4532447ad87efc9697e
김지혜 <137058764+kimjihye058@users.noreply.github.com>
2024-10-05 23:51:12 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.04 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        int answer = 0;
+        
+        for (int i = 0; i < included.length; i++) {
+            if (included[i]) {
+                answer += a + (i * d);
+                continue;
+            }
+        }
+        
+        return answer;
+    }
+}
